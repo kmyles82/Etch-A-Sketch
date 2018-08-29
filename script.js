1b575ebb1792371f2839ed89d99ae6f413b3910e
kmyles82 <kmyles82@gmail.com>
2018-08-29 16:04:23 -0500

    added functionality to reset grid

--- script.js
+++ script.js
@@ -2,25 +2,54 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
 container.id = 'container'
+const button = document.createElement('button')
+button.id = 'reset'
+button.textContent = 'Reset Grid'
+body.appendChild(button)
 body.appendChild(container);
 
 //create and append grid divs to container div
 const createGrid = function (num) {
-    for (var j = 1; j <= num; j++) {
+    let num_squares = num
+    for (var j = 1; j <= num_squares; j++) {
         const grid = document.createElement('div');
         grid.className = 'grid';
         grid.id = 'grid' + j;
         container.appendChild(grid);
+       
+        grid.addEventListener('click', function (e) {
+            this.style.background = 'black';
+          });
+      
+        grid.addEventListener('mousemove', function (e) {
+        //left mouse button down
+            if (e.buttons == 1) {
+            console.log(e)
+            this.style.background = 'black';
+        }
+        //right mouse button down
+        if (e.buttons == 2) {
+            this.style.background = 'blue';
+        }
+        });
     }
 }
-
-//jayout grid across entire canvas equally
+ //resets the grids
+ let clearGrid = document.querySelector('#reset');
+ clearGrid.addEventListener('click', function () {
+    let grids = prompt("how many");
+    layoutGrid(grids) 
+}) 
+ 
+//layout grid across entire canvas equally
 const layoutGrid = function (num) { 
-    for (var i = 0; i <= num; i++){
-        createGrid(16);
+    for (var i = 0; i < num; i++){
+        createGrid(num);
     }
 }
 layoutGrid(16)
+
+
 
 
 
