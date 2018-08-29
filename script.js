cedc3ca21103ad7179e042f25fa509c2b9de7664
kmyles82 <kmyles82@gmail.com>
2018-08-29 13:59:48 -0500

    added css styling and javascript to render grids

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 //Create and append container div to body
 const body = document.querySelector('body');
-const div = document.createElement('div');
-div.id = 'container'
-body.appendChild(div);
+const container = document.createElement('div');
+container.id = 'container'
+body.appendChild(container);
+
 //create and append grid divs to container div
-const grid = document.createElement('div')
-grid.id = 'grid'
-div.appendChild(grid)
+const createGrid = function (num) {
+    for (var j = 1; j <= num; j++) {
+        const grid = document.createElement('div');
+        grid.className = 'grid';
+        grid.id = 'grid' + j;
+        container.appendChild(grid);
+    }
+}
+
+//jayout grid across entire canvas equally
+const layoutGrid = function (num) { 
+    for (var i = 0; i <= num; i++){
+        createGrid(16);
+    }
+}
+layoutGrid(16)
+
 
 
